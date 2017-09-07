69c7678f353150a9e3c9fb32324b2b37a6ec4316
Casey Watts <casey.s.watts@gmail.com>
2017-09-07 17:42:50 -0400

    extract out meetupUrl variable

--- src/ui/components/meetup-card-list/component.ts
+++ src/ui/components/meetup-card-list/component.ts
@@ -2,6 +2,8 @@ import Component, { tracked } from '@glimmer/component';
 import EventPresenter from './EventPresenter';
 import fetchMeetups from './fetchMeetups'
 
+const meetupUrl = "https://api.meetup.com/2/events?offset=0&format=json&limited_events=False&group_urlname=techcorridorio&page=200&fields=&order=time&desc=false&status=upcoming&sig_id=168857872&sig=e659cc6038d27adf6eae600a44905c69196c77df";
+
 export default class MeetupCardList extends Component {
   @tracked events
 
@@ -15,7 +17,7 @@ export default class MeetupCardList extends Component {
   }
 
   loadMeetups() {
-    fetchMeetups("https://api.meetup.com/2/events?offset=0&format=json&limited_events=False&group_urlname=techcorridorio&page=200&fields=&order=time&desc=false&status=upcoming&sig_id=168857872&sig=e659cc6038d27adf6eae600a44905c69196c77df").then((responseData) => {
+    fetchMeetups(meetupUrl).then((responseData) => {
       const someEvents = responseData.results.slice(0, this.numOfCardsToShow);
       const decoratedMeetupEvents = someEvents.map((rawEventData) => {
         return EventPresenter(rawEventData);
