129ad99e101074d2be4b8ae25c003513a126dbab
Casey Watts <casey.s.watts@gmail.com>
2017-09-07 17:18:24 -0400

    import code from arlingtonruby-react

--- src/ui/components/meetup-card-list/component.ts
+++ src/ui/components/meetup-card-list/component.ts
@@ -1,9 +1,23 @@
-import Component from '@glimmer/component';
+import Component, { tracked } from '@glimmer/component';
+import EventPresenter from './EventPresenter';
+import fetchMeetups from './fetchMeetups'
 
 export default class MeetupCardList extends Component {
-  events = [
-    {text: 'a'},
-    {text: 'b'},
-    {text: 'c'}
-  ];
+  numOfCardsToShow = 5;
+  @tracked events
+
+  constructor(options) {
+    super(options);
+    this.loadMeetups();
+  }
+
+  loadMeetups() {
+    fetchMeetups("https://api.meetup.com/2/events?offset=0&format=json&limited_events=False&group_urlname=techcorridorio&page=200&fields=&order=time&desc=false&status=upcoming&sig_id=168857872&sig=e659cc6038d27adf6eae600a44905c69196c77df").then((responseData) => {
+      const someEvents = responseData.results.slice(0, this.numOfCardsToShow);
+      const decoratedMeetupEvents = someEvents.map((rawEventData) => {
+        return EventPresenter(rawEventData);
+      })
+      this.events = decoratedMeetupEvents;
+    })
+  }
 };
--- src/ui/components/meetup-card/component.ts
+++ src/ui/components/meetup-card/component.ts
@@ -1,5 +1,4 @@
 import Component from '@glimmer/component';
 
 export default class MeetupCard extends Component {
-  // text = this.props.text || 'lol';
 };
