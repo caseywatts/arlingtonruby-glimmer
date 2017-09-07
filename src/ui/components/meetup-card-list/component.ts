59f0e60d57e5f49cd0ed7ac6b38e6a817be1a01f
Casey Watts <casey.s.watts@gmail.com>
2017-09-07 16:58:57 -0400

    iterate over a list

---
+++ src/ui/components/meetup-card-list/component.ts
@@ -0,0 +1,9 @@
+import Component from '@glimmer/component';
+
+export default class MeetupCardList extends Component {
+  events = [
+    'a',
+    'b',
+    'c'
+  ];
+};
---
+++ src/ui/components/meetup-card/component.ts
@@ -0,0 +1,5 @@
+import Component from '@glimmer/component';
+
+export default class MeetupCard extends Component {
+  // text = this.props.text || 'lol';
+};
